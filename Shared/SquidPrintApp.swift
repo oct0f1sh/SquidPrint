ed9054ef4c83f42b9a2d1117a6198555af94e4a5
Duncan <buzzbuzz.macdonald@gmail.com>
2020-06-26 17:38:14 -0700

    Initial Commit

---
+++ Shared/SquidPrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquidPrintApp.swift
+//  Shared
+//
+//  Created by Duncan MacDonald on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct SquidPrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
