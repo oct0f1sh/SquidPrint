a0901d1c418d181959a1b09722731e29f66eec64
Duncan <buzzbuzz.macdonald@gmail.com>
2020-06-28 18:39:04 -0700

    Initial commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,7 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            Sidebar()
+            Dashboard()
+        }
+    }
+}
+
+struct Dashboard: View {
+    var body: some View {
+        VStack {
+            Text("Dashboard").font(.largeTitle).fontWeight(.bold)
+            Spacer()
+            HStack {
+                Spacer()
+                Controls()
+                Spacer()
+            }
+            Spacer()
+            Spacer()
+        }
+    }
+}
+
+struct Control: View {
+    var title: String
+    
+    var body: some View {
+        NavigationLink(destination: Text("Good job")) {
+            Label {
+                Text(title)
+            } icon: {
+                Image(systemName: "wand.and.rays")
+            }.labelStyle(TitleOnlyLabelStyle())
+        }
+    }
+}
+
+struct Controls: View {
+    var body: some View {
+        List (1..<5 ) { i in
+            Control(title: "Control \(i)")
+        }
+    }
+}
+
+struct Sidebar: View {
+    var body: some View {
+        List( 1..<100 ) { i in
+            HStack {
+                Image(systemName: "flame.fill")
+            }
+            Text("Row \(i)")
+        }
+        .navigationTitle("SquidPrint")
+        .listStyle(SidebarListStyle())
     }
 }
 
