4555d8bcd10b144389cd9b364254dc3f13c880b1
Duncan <buzzbuzz.macdonald@gmail.com>
2020-07-18 19:34:05 -0700

    Added QR code scanner

--- Shared/Views/AddPrinterView.swift
+++ Shared/Views/AddPrinterView.swift
@@ -7,19 +7,35 @@
 
 import Foundation
 import SwiftUI
+import CodeScanner
 
 struct AddPrinterView: View {
     @State private var printerName: String = ""
     @State private var printerURL: String = ""
     @State private var printerAPIKey: String = ""
     
+    @State private var isPresentingQRScanner: Bool = false
+    
     var handler: (_ name: String, _ url: String, _ apiKey: String) -> Void
     
     var body: some View {
-        Group {
+        VStack {
             Text("New Printer").font(.title).padding(.top, 20)
             Form {
-                Section {
+                Section(footer:
+                            HStack {
+                                Spacer()
+                                Button(action: { handler(printerAPIKey, printerURL, printerAPIKey) }, label: {
+                                    Text("Add printer")
+                                })
+                                .padding()
+                                .frame(minWidth: 100)
+                                .foregroundColor(.white)
+                                .background(Color.accentColor)
+                                .cornerRadius(10)
+                                Spacer()
+                            }.padding(.top, 10)
+                ) {
                     HStack {
                         Text("Name: ")
                         TextField("Prusa i3 Mk3", text: $printerName)
@@ -31,17 +47,37 @@ struct AddPrinterView: View {
                     HStack {
                         Text("API Key: ")
                         TextField("API KEY", text: $printerAPIKey)
+                        Button(action: {
+                            isPresentingQRScanner = true
+                        }, label: {
+                            Image(systemName: "qrcode.viewfinder").font(.title)
+                        })
                     }
                 }
             }
-            
-            Button(action: { handler(printerName, printerURL, printerAPIKey) }, label: {
-                Text("Add printer")
-            })
-            .padding()
-            .foregroundColor(.white)
-            .background(Color.accentColor)
-            .cornerRadius(10)
+        }.sheet(isPresented: $isPresentingQRScanner) {
+            CodeScannerView(codeTypes: [.qr], completion: handleQRScan(result:))
+        }
+    }
+    
+    func handleQRScan(result: Result<String, CodeScannerView.ScanError>) {
+        isPresentingQRScanner = false
+        
+        switch result {
+        case .success(let code):
+            print(code)
+            printerAPIKey = code
+        case .failure(let error):
+            print(error)
+            printerAPIKey = "error scanning..."
+        }
+    }
+}
+
+struct AddPrinterView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPrinterView { _, _, _ in
+            ()
         }
     }
 }
